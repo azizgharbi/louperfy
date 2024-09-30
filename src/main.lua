3bba8a854c82ea4edf50e5cb5a4dc6a5bedaad26
flava <flava-machine@flavas-MacBook-Pro.local>
2024-09-30 10:49:32 -0400

    feat: init project wip

--- src/main.lua
+++ src/main.lua
@@ -1,51 +1,31 @@
-local log_path = "test.txt" -- Change this to your log file path
-local running = true -- Flag to control the loop
+local running = true
+local last_size = 0
 
--- Function to read the log file and print its contents
-local function read_log()
-	local file = io.open(log_path, "r")
+local function read_new_lines(file_path)
+	local file = io.open(file_path, "r")
 	if not file then
-		print("Error opening log file.")
 		return
 	end
 
-	-- Read all lines in the log file
-	for line in file:lines() do
-		print(line)
-	end
+	-- Seek to the last known position
+	file:seek("set", last_size)
+
+	local new_lines = file:read("*a") -- Read all new content
+	last_size = file:seek() -- Update last_size
 
 	file:close()
+
+	if new_lines and new_lines ~= "" then
+		print("New lines: " .. new_lines)
+	end
 end
 
--- Function to monitor the log file for new lines
-local function monitor_log()
-	local file = io.open(log_path, "r")
-	if not file then
-		print("Error opening log file.")
-		return
-	end
+local file_path = "test.txt"
 
-	-- Seek to the end of the file
-	file:seek("end")
-
-	while running do
-		local line = file:read("*all")
-		if line then
-			print(line)
-		else
-			-- Sleep for a short duration to avoid busy waiting
-			os.execute("sleep 2") -- Adjust sleep duration as necessary
-		end
-	end
-
-	file:close()
+while running do
+	read_new_lines(file_path)
+	os.execute("sleep 1") -- Adjust sleep duration as necessary
 end
-
--- Read existing log lines
-read_log()
-
--- Start monitoring the log file for new lines
-local monitor_thread = coroutine.create(monitor_log)
 
 -- Allow for graceful exit
 while running do
@@ -54,6 +34,3 @@ while running do
 		running = false
 	end
 end
-
--- Resume the monitor thread to clean up
-coroutine.resume(monitor_thread)
